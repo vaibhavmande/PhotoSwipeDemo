5e4f3453e17bffdcb5fe96ea4215b8a3eea33b63
vaibhavmande <vaibhavrmande@gmail.com>
2019-08-19 10:34:57 +0530

    Disable touch events

--- dist/photoswipe-ui-default.js
+++ dist/photoswipe-ui-default.js
@@ -1,4 +1,4 @@
-/*! PhotoSwipe Default UI - 4.1.3 - 2019-08-18
+/*! PhotoSwipe Default UI - 4.1.3 - 2019-08-19
 * http://photoswipe.com
 * Copyright (c) 2019 Dmitry Semenov; */
 /**
--- dist/photoswipe.js
+++ dist/photoswipe.js
@@ -1,4 +1,4 @@
-/*! PhotoSwipe - v4.1.3 - 2019-08-18
+/*! PhotoSwipe - v4.1.3 - 2019-08-19
 * http://photoswipe.com
 * Copyright (c) 2019 Dmitry Semenov; */
 (function (root, factory) { 
