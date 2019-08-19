967aae53027f12b5af1a50a1186980d910d0cf55
vaibhavmande <vaibhavrmande@gmail.com>
2019-08-19 11:48:25 +0530

    Debug hyperlinks 4

--- _site/dist/photoswipe-ui-default.js
+++ _site/dist/photoswipe-ui-default.js
@@ -77,8 +77,8 @@ var PhotoSwipeUI_Default =
 			clickToCloseNonZoomable: true,
 
 			shareButtons: [
-				{id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/'},
-				{id:'twitter', label:'Tweet', url:'https://twitter.com/'},
+				{id:'facebooks', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'},
+				{id:'twitters', label:'Tweet', url:'https://twitter.com/intent/tweet?text={{text}}&url={{url}}'},
 				{id:'pinterest', label:'Pin it', url:'http://www.pinterest.com/pin/create/button/'+
 													'?url={{url}}&media={{image_url}}&description={{text}}'},
 				{id:'download', label:'Download image', url:'{{raw_image_url}}', download:true}
